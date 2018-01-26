be8cff2d5b59bea900f45767d4b7b61aa0050ece
wangjue <wangjue1199@163.com>
2018-01-26 21:30:40 +0800

    pitch change tile count

--- src/layers/tileLayer/tile-grid.js
+++ src/layers/tileLayer/tile-grid.js
@@ -2,7 +2,8 @@ import MathUtils from '../../util/MathUtils';
 import tileCache from './tile-cache';
 import Tile from './tile';
 
-let lastTile, lastTiles, lastRange = 0;
+let lastVisibleExtent, visibleTiles;
+const EPS = 0.001;
 
 // 正常球面纬度 转换为 墨卡托投影球面纬度
 // -PI/2 < phi < PI/2
@@ -63,70 +64,83 @@ function reviseRowAndCol(size, row, col) {
     return [row, col];
 }
 
-function getRoundTiles(tile, range) {
+// 根据视线视角计算可视切片
+function getRoundTiles(tile, pitch, bearing) {
     const offsetY = Math.abs(tile.row / tile.size - 0.5) * 10;
-    const roundSize = Math.floor((tile.zoom < 5 ? tile.zoom + 3 : 4 + offsetY) * range);
+    const pitchRatio = 270 / (270 - pitch);
+    // 靠近高纬度、倾斜角大的情况下，可视切片数量大
+    const roundSize = Math.floor(
+        (tile.zoom < 4 ? tile.zoom + 3 : 4 + offsetY) * pitchRatio
+    );
+
+    const centerRow = Math.round(tile.row + Math.cos(bearing) * (pitchRatio - 1) * tile.zoom * tile.zoom * 0.05),
+        centerCol = Math.round(tile.col + Math.sin(bearing) * (pitchRatio - 1) * tile.zoom * tile.zoom * 0.05);
     const tiles = [],
         tileIds = [];
     for (let dr = 0; dr < roundSize; dr++) {
         for (let dc = 0; dc < roundSize; dc++) {
-            let row_col = reviseRowAndCol(tile.size, tile.row + dr, tile.col + dc);
-            let t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
-            if (tileIds.indexOf(t.id) < 0) {
+            if (tiles.length > 100 * pitchRatio) break;
+            let row_col = reviseRowAndCol(tile.size, centerRow + dr, centerCol + dc);
+            let _id = tile.zoom + '-' + row_col[1] + '-' + row_col[0];
+            if (tileIds.indexOf(_id) < 0) {
+                let t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
                 tileIds.push(t.id);
                 tiles.push(t);
             }
 
             if (dc > 0) {
-                row_col = reviseRowAndCol(tile.size, tile.row + dr, tile.col - dc);
-                t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
-                if (tileIds.indexOf(t.id) < 0) {
+                row_col = reviseRowAndCol(tile.size, centerRow + dr, centerCol - dc);
+                _id = tile.zoom + '-' + row_col[1] + '-' + row_col[0];
+                if (tileIds.indexOf(_id) < 0) {
+                    let t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
                     tileIds.push(t.id);
                     tiles.push(t);
                 }
             }
             if (dr > 0) {
-                row_col = reviseRowAndCol(tile.size, tile.row - dr, tile.col + dc);
-                t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
-                if (tileIds.indexOf(t.id) < 0) {
+                row_col = reviseRowAndCol(tile.size, centerRow - dr, centerCol + dc);
+                _id = tile.zoom + '-' + row_col[1] + '-' + row_col[0];
+                if (tileIds.indexOf(_id) < 0) {
+                    let t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
                     tileIds.push(t.id);
                     tiles.push(t);
                 }
             }
             if (dc > 0 && dr > 0) {
-                row_col = reviseRowAndCol(tile.size, tile.row - dr, tile.col - dc);
-                t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
-                if (tileIds.indexOf(t.id) < 0) {
+                row_col = reviseRowAndCol(tile.size, centerRow - dr, centerCol - dc);
+                _id = tile.zoom + '-' + row_col[1] + '-' + row_col[0];
+                if (tileIds.indexOf(_id) < 0) {
+                    let t = genTile(tile.radius, tile.zoom, tile.size, row_col[1], row_col[0], tile.width);
                     tileIds.push(t.id);
                     tiles.push(t);
                 }
             }
         }
     }
-    console.log(tile.zoom, roundSize, tiles.length, tileCache.size());
+    console.log(tile.zoom, tile.row, tile.col, centerRow, centerCol, roundSize, tiles.length, pitchRatio);
     return tiles;
 }
 
 export default {
-    getVisibleTiles: function (radius, zoom, phi, theta, range) {
-        if (this.using) return false;
-        this.using = true;
-
+    getVisibleTiles: function (radius, zoom, phi, theta, pitch, bearing) {
         var tile = getTile(radius, zoom, phi, theta);
-        if (lastTile && lastTile.id === tile.id && Math.abs(lastRange - range) < 0.1) {
-            this.using = false;
+
+        if (lastVisibleExtent && tile.id === lastVisibleExtent.id &&
+            Math.abs(pitch - lastVisibleExtent.pitch) < EPS &&
+            Math.abs(bearing - lastVisibleExtent.bearing) < EPS) {
             return false;
         }
-        lastTile = tile;
-        lastRange = range;
 
-        var tiles = getRoundTiles(tile, range);
-        lastTiles = tiles;
+        lastVisibleExtent = {
+            id: tile.id,
+            pitch: pitch,
+            bearing: bearing
+        };
 
-        this.using = false;
-        return tiles;
+        visibleTiles = getRoundTiles(tile, pitch, bearing);
+        return visibleTiles;
     },
     isVisible: function (tileId) {
-        return lastTiles && lastTiles.find(tile => tile.id === tileId);
+        return visibleTiles && visibleTiles.find(tile => tile.id === tileId);
     }
 };
